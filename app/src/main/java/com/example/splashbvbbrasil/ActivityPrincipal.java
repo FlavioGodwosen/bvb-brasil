c70e0cb7bd4f56a1e7714bc559489ee12e22d9aa
Flavio Silva <flavio131983@gmail.com>
2019-05-21 10:01:14 -0300

    Commit do Projeto

---
+++ app/src/main/java/com/example/splashbvbbrasil/ActivityPrincipal.java
@@ -0,0 +1,15 @@
+package com.example.splashbvbbrasil;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityPrincipal extends Activity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            setContentView(R.layout.activity_main);
+        }
+    }
+
---
+++ app/src/main/java/com/example/splashbvbbrasil/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.splashbvbbrasil;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashActivity.this, ActivityPrincipal.class);
+                startActivity(i);
+
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
+
+
+
+
