ec2c9bf61fb43671bf50a3d64f628cdd214974d5
Flavio <flavio131983@gmail.com>
2019-11-13 21:30:22 -0300

    Novas views para teste.

--- app/src/main/java/com/bvbbr/bvb_brasil/ActivityPrincipal.java
+++ app/src/main/java/com/bvbbr/bvb_brasil/ActivityPrincipal.java
@@ -3,10 +3,23 @@ package com.bvbbr.bvb_brasil;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
+import static android.content.ContentValues.TAG;
 
 public class ActivityPrincipal extends Activity {
 
@@ -15,6 +28,27 @@ public class ActivityPrincipal extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+         myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", Objects.requireNonNull(error).toException());
+            }
+        });
+
         HorizontalScrollView Banners = findViewById(R.id.ScrollView1);
         final ImageView Banners1 = findViewById(R.id.bgImage4);
         final ImageView Banners2 = findViewById(R.id.bgImage1);
@@ -22,11 +56,11 @@ public class ActivityPrincipal extends Activity {
         //ImageView.setMovementMethod(new ScrollingMovementMethod());
 
 
-        Button Bundesliga = (Button) findViewById(R.id.Bundeslig);
-        Button SuperCopa = (Button) findViewById(R.id.SuperCopa);
-        Button Pokal = (Button) findViewById(R.id.Pokal);
-        Button ChampionsL = (Button) findViewById(R.id.ChampionsL);
-        Button Encontros = (Button) findViewById(R.id.Encontros);
+        Button Bundesliga = findViewById(R.id.Bundeslig);
+        Button SuperCopa = findViewById(R.id.SuperCopa);
+        Button Pokal = findViewById(R.id.Pokal);
+        Button ChampionsL = findViewById(R.id.ChampionsL);
+        Button Encontros = findViewById(R.id.Encontros);
 
         Bundesliga.setOnClickListener(new View.OnClickListener() {
 
